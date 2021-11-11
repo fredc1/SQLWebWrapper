c7080f29a18affab8b5ab8e0f432e85fdaa38c65
Frederick Cunningham <frederick.cunningham98@gmail.com>
2021-11-11 03:45:11 +0000

    runs on the server to public IP!

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def index():
 
 
 def main():
-    # app.run(host="0.0.0.0", port=8111, debug=False)
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8111, debug=False)
+    #app.run(host="127.0.0.1", port=8080, debug=True)
 
 
 if __name__ == '__main__':
